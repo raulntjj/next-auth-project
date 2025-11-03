1e0706694eb209b2ecba0d59227d08a0da45a7ee
raulntjj <rauloliveira5230@gmail.com>
2025-11-03 16:06:11 -0300

    fix: add PrismaNeon adapter to PrismaClient initialization and update dependencies

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from "@prisma/client"
+import { PrismaNeon } from "@prisma/adapter-neon"
+
+const DATABASE_URL = process.env.DATABASE_URL
+
+const adapter = new PrismaNeon({ connectionString: DATABASE_URL })
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
